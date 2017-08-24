4cc1522cee9673f094de29b34b053ff26ad4e393
Justin Kim <justin.kim@collabora.com>
2017-08-24 13:39:44 +0900

    sctp: Check if NULL before calling g_thread_join

--- ext/sctp/sctpassociation.c
+++ ext/sctp/sctpassociation.c
@@ -198,7 +198,9 @@ static void gst_sctp_association_finalize(GObject *object)
     }
     G_UNLOCK(associations_lock);
 
-    g_thread_join(self->connection_thread);
+    if (self->connection_thread != NULL) {
+      g_thread_join(self->connection_thread);
+    }
 
     G_OBJECT_CLASS(gst_sctp_association_parent_class)->finalize(object);
 }
